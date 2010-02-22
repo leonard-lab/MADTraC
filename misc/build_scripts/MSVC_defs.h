3ee8cf25ac44c588189e80bfa59b21a122947144
Dan Swain <dan.t.swain@gmail.com>
2010-02-22 22:01:12 +0000

    Add VS2010 project files, including entries in misc/build_scripts. Set some ignore properties.
    git-svn-id: svn+ssh://poincare.princeton.edu/svn/repos/MADTraC/trunk@4 a658d305-d20c-4271-b950-7aa421b211f3

---
+++ misc/build_scripts/MSVC_defs.h
@@ -0,0 +1,35 @@
+#ifdef __MSC_VER
+/* Manual set up of definitions for Visual Studio
+   (unless you can figure out a way to export the
+    preprocessor definitions to a post-build event
+    script... let me know!) */
+
+#ifndef WIN32
+#define WIN32
+#endif
+
+#ifndef _WINDOWS
+#define _WINDOWS
+#endif
+
+#ifndef WINVER
+#define WINVER 0x0400
+#endif
+
+#ifndef __WXMSW__
+#define __WXMSW__
+#endif
+
+#ifndef wxUSE_GUI
+#define wxUSE_GUI
+#endif
+
+#ifndef _CRT_SECURE_NO_DEPRECATE
+#define _CRT_SECURE_NO_DEPRECATE
+#endif
+
+#ifndef _CRT_NONSTDC_NO_DEPRECATE
+#define _CRT_NONSTDC_NO_DEPRECATE
+#endif
+
+#endif /* _MSC_VER
