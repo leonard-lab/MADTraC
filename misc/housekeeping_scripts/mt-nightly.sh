9f9cb85347eaed734fecf370abbb91cfbe199349
Dan Swain <dan.t.swain@gmail.com>
2010-07-13 19:41:08 +0000

    [misc, CameraUtility] Fixed nightly tarball and zips in mt-nightly.sh.  Added Mac stuff to CameraUtility CMake.
    git-svn-id: svn+ssh://poincare.princeton.edu/svn/repos/MADTraC/trunk@108 a658d305-d20c-4271-b950-7aa421b211f3

--- misc/housekeeping_scripts/mt-nightly.sh
+++ misc/housekeeping_scripts/mt-nightly.sh
@@ -3,6 +3,7 @@
 # MADTraC nightly script running currently on poincare.princeton.edu
 MT_DIR=~/MT_Safe/MADTraC
 MT_WEB_DIR=/var/www/poincare/MADTraC
+EXCLUDES_FILE=$MT_DIR/../excludes.txt
 
 echo =======================================================
 echo === Log of mt-nightly for `date`
@@ -13,16 +14,21 @@ svn update
 cd $MT_DIR/docs/doxygen
 echo === Creating documentation
 doxygen Doxyfile
-echo === Creating nightly tarball and zip
+echo === Creating nightly tarball
 cd $MT_DIR
 cd ..
-tar -pczf $MT_WEB_DIR/latest.tar.gz MADTraC
-zip -r $MT_WEB_DIR/latest.zip MADTraC
+find MADTraC -name "*.svn*" > $EXCLUDES_FILE
+echo excludes.txt >> $EXCLUDES_FILE
+echo *.log >> $EXCLUDES_FILE
+echo *docs/html/* >> $EXCLUDES_FILE
+rm -f $MT_WEB_DIR/latest.tar.gz
+rm -f $MT_WEB_DIR/latest.zip
+tar -pczf $MT_WEB_DIR/latest.tar.gz --exclude-from="$EXCLUDES_FILE" MADTraC
+echo === Creating nightly zip
+zip -r $MT_WEB_DIR/latest.zip * -x '*.svn*' excludes.txt '*.log' *docs/html/*
 echo === Copying to server
 cp -R $MT_DIR/web/* $MT_WEB_DIR
 cp -R $MT_DIR/docs/html/* $MT_WEB_DIR/docs
 
 echo === End of log for `date` 
 echo =======================================================
-
-
