b4ad8a6d4c1b210f46526ac31b8001221ee8533b
Dan Swain <dan.t.swain@gmail.com>
2010-07-13 13:49:56 +0000

    [misc] Added nightly tarball and zip.
    git-svn-id: svn+ssh://poincare.princeton.edu/svn/repos/MADTraC/trunk@104 a658d305-d20c-4271-b950-7aa421b211f3

--- misc/housekeeping_scripts/mt-nightly.sh
+++ misc/housekeeping_scripts/mt-nightly.sh
@@ -13,6 +13,9 @@ svn update
 cd $MT_DIR/docs/doxygen
 echo === Creating documentation
 doxygen Doxyfile
+echo === Creating nightly tarball and zip
+tar -pczf $MT_WEB_DIR/latest.tar.gz $MT_DIR/*
+zip -r $MT_WEB_DIR/latest.zip $MT_DIR/*
 echo === Copying to server
 cp -R $MT_DIR/web/* $MT_WEB_DIR
 cp -R $MT_DIR/docs/html/* $MT_WEB_DIR/docs
