0e55716f048bd42c88109bc356ed198d509460a6
Valeri Karpov <vkarpov@princeton.edu>
2010-07-19 16:13:06 +0000

    Added a call to doQuit in the destructor so when the user hits the exit button on Windows the XML data gets saved.
    git-svn-id: svn+ssh://poincare.princeton.edu/svn/repos/MADTraC/trunk@121 a658d305-d20c-4271-b950-7aa421b211f3

--- MT/MT_Tracking/base/MT_TrackerFrameBase.cpp
+++ MT/MT_Tracking/base/MT_TrackerFrameBase.cpp
@@ -142,6 +142,7 @@ MT_TrackerFrameBase::~MT_TrackerFrameBase()
         delete m_pTracker;
         m_pTracker = NULL;
     }
+	MT_FrameBase::doQuit();
 }
 
 /********************************************************************/
